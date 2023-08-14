a7674de1c43beeccc142b4caf7fe8947ec896821
Arshadpatel002 <113446075+Arshadpatel002@users.noreply.github.com>
2023-08-14 13:24:37 +0530

    Delete index.js

--- js123/index.js
+++
@@ -1,9 +0,0 @@
-let sum=0
-console.log("Enter the value of n")
-console.log("Enter the value of n")
-
-let n = Number.parseInt(n)
-for(let i=0;i<=n;i++){
-    sum += i;
-} 
-console .log("Sum of First "+n+" natural number is "+ sum)
